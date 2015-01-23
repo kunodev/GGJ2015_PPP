ff3e5b87de9e2197afae14655c3992c891842717
Kuno <tonnos@gmx.de>
2015-01-23 22:52:01 +0100

    testing....

--- core/src/com/mygdx/game/PowerfulPandaApp.java
+++ core/src/com/mygdx/game/PowerfulPandaApp.java
@@ -1,27 +1,45 @@
 package com.mygdx.game;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+
+import de.panda.tiled.MapRenderer;
 
 public class PowerfulPandaApp extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	MapRenderer renderer;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		AssetManager manager = new AssetManager();
+		manager.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
+		manager.load("stage_test.tmx", TiledMap.class);
+		manager.finishLoading();
+
+		OrthographicCamera cam = new OrthographicCamera();
+
+		renderer = new MapRenderer("stage_test.tmx", cam, manager);
+		renderer.loadComponentsFromMap(new Engine());
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		renderer.render();
 		batch.end();
 	}
 }
---
+++ core/src/de/panda/tiled/MapRenderer.java
@@ -0,0 +1,110 @@
+package de.panda.tiled;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.reflections.Reflections;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class MapRenderer {
+
+	public TiledMapRenderer renderer;
+
+	public TiledMap map;
+
+	public OrthographicCamera cam;
+
+	public static class ComponentRegistry {
+		Reflections ref = new Reflections("");
+
+		public Map<String, Class<? extends Component>> classNameToClass;
+
+		public ComponentRegistry() {
+			Set<Class<? extends Component>> components = this.ref.getSubTypesOf(Component.class);
+			this.classNameToClass = new HashMap<String, Class<? extends Component>>();
+			for (Class<? extends Component> clazz : components) {
+				this.classNameToClass.put(clazz.getSimpleName(), clazz);
+			}
+		}
+	}
+
+	public MapRenderer(String file, OrthographicCamera cam, AssetManager assetMan) {
+		this.cam = cam;
+		map = assetMan.get("stage_test.tmx");
+		this.renderer = new OrthogonalTiledMapRenderer(map);
+		int width = (Integer) map.getProperties().get("width");
+		int tileWidth = (Integer) map.getProperties().get("tilewidth");
+		int height = (Integer) map.getProperties().get("height");
+		int tileHeight = (Integer) map.getProperties().get("tileheight");
+		cam.setToOrtho(false, width * tileWidth, height * tileHeight);
+	}
+
+	public void loadComponentsFromMap(Engine engine) {
+		ComponentRegistry components = new ComponentRegistry();
+		for (MapLayer layer : map.getLayers()) {
+			if (layer instanceof TiledMapTileLayer) {
+				TiledMapTileLayer tileLayer = (TiledMapTileLayer) layer;
+				for (int i = 0; i < tileLayer.getWidth(); i++) {
+					for (int j = 0; j < tileLayer.getHeight(); j++) {
+						Entity e = new Entity();
+						Cell c = tileLayer.getCell(i, j);
+						try {
+							Iterator<String> keys = c.getTile().getProperties().getKeys();
+							createComponent(components, e, keys);
+
+						} catch (NullPointerException ex) {
+							// Shit happens
+						}
+					}
+				}
+			}
+			for (MapObject obj : layer.getObjects()) {
+				if (obj instanceof RectangleMapObject) {
+					RectangleMapObject robj = (RectangleMapObject) obj;
+					Entity e = new Entity();
+					Iterator<String> keys = robj.getProperties().getKeys();
+					createComponent(components, e, keys);
+				}
+			}
+
+		}
+	}
+
+	private void createComponent(ComponentRegistry components, Entity e, Iterator<String> keys) {
+		while (keys.hasNext()) {
+			String key = keys.next();
+			Class<? extends Component> clazz = components.classNameToClass.get(key);
+			try {
+				if (clazz != null) {
+					e.add(clazz.newInstance());
+				}
+			} catch (InstantiationException e1) {
+				e1.printStackTrace();
+			} catch (IllegalAccessException e1) {
+				e1.printStackTrace();
+			}
+			System.out.println("Something has a property!");
+		}
+	}
+
+	public void render() {
+		cam.update();
+		renderer.setView(cam);
+		renderer.render();
+	}
+}
