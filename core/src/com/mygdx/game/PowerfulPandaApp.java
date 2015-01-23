7a7fe710be793ca71d1cba3d32b9fa6dbf17a2b5
flo <florian.kaluschke@gmail.com>
2015-01-23 22:22:44 +0100

    first dumb commit

--- core/src/com/mygdx/game/PowerfulPandaApp.java
+++ core/src/com/mygdx/game/PowerfulPandaApp.java
@@ -1,27 +1,29 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class PowerfulPandaApp extends ApplicationAdapter {
-	SpriteBatch batch;
+public class PowerfulPandaApp extends Game {
+	public SpriteBatch batcher;
 	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		batcher = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		GL20 gl = Gdx.gl;
+		gl.glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
+		gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		super.render();
 	}
 }
